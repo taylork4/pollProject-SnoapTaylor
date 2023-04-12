2e8a824bd4e167a993cc0db30133902c6fabd6af
snoapca <snoapca@Ubuntu22.04.myguest.virtualbox.org>
2023-04-12 17:25:36 -0400

    added poll page functionality

--- src/components/Polls.vue
+++ src/components/Polls.vue
@@ -7,8 +7,9 @@ import { User } from "firebase/auth";
 import { ref, defineProps, computed, withDefaults, Ref, watch } from "vue"
 import { useRoute, RouteLocationNormalized } from 'vue-router';
 import 'firebase/firestore';
-import {collection, addDoc, DocumentReference, DocumentSnapshot, setDoc, doc, getDoc, getDocs, CollectionReference, query, collectionGroup, QuerySnapshot, where, QueryDocumentSnapshot } from 'firebase/firestore';
+import { collection, addDoc, DocumentReference, DocumentSnapshot, setDoc, doc, getDoc, getDocs, CollectionReference, query, collectionGroup, QuerySnapshot, where, QueryDocumentSnapshot } from 'firebase/firestore';
 import { db, auth } from '../firebase/init.js'
+import { stringLength } from "@firebase/util";
 
 const dt = new Date();
 const dt_string = dt.toLocaleString();
@@ -17,18 +18,18 @@ const pollUser = doc(db, 'polls/user')
 const pollPublic = doc(db, 'polls/public')
 // const wordsColl = doc(db, 'wordleWords/words')
 let pollData: {
-    pollQuestion: string;
-    pollChoices: Array<string>;
-    votes: Array<number>;
-    date: string;
-    pollsCreated: number;
-  } = {
-    pollQuestion: "",
-    pollChoices: ["", "", "", ""],
-    votes: [0, 0, 0, 0],
-    date: "",
-    pollsCreated: 0
-  }
+  pollQuestion: string;
+  pollChoices: Array<string>;
+  votes: Array<number>;
+  date: string;
+  pollsCreated: number;
+} = {
+  pollQuestion: "",
+  pollChoices: ["", "", "", ""],
+  votes: [0, 0, 0, 0],
+  date: "",
+  pollsCreated: 0
+}
 
 
 // define the type for the current route object
@@ -50,17 +51,17 @@ let numPolls: number = 0;
 let pollCount: number = 1;
 
 const isLoggedIn = ref(true)
-  // runs after firebase is initialized
-  auth.onAuthStateChanged(function(user: User | null) {
-      if (user) {
-        isLoggedIn.value = true // if we have a user
-        userUid.value = user.uid; // store the user UID in the ref
-        console.log(`User UID: ${userUid.value}`);        
-      } else {
-        isLoggedIn.value = false // if we do not
-        userUid.value = ''; // clear the user UID ref
-      }
-  })
+// runs after firebase is initialized
+auth.onAuthStateChanged(function (user: User | null) {
+  if (user) {
+    isLoggedIn.value = true // if we have a user
+    userUid.value = user.uid; // store the user UID in the ref
+    console.log(`User UID: ${userUid.value}`);
+  } else {
+    isLoggedIn.value = false // if we do not
+    userUid.value = ''; // clear the user UID ref
+  }
+})
 
 async function logUserUid() {
   await new Promise<void>((resolve) => {
@@ -85,20 +86,20 @@ function setUserId(user: User | null) {
 }
 
 async function setFire(coll: DocumentReference, data: any) {
-    try {
-        await setDoc(coll, data);
-        // await addDoc(collection(coll, email as string), data)
-        console.log("Successful addition!");
-    } catch (error) {
-        console.log(`I got an error! ${error}`);
-    }
+  try {
+    await setDoc(coll, data);
+    // await addDoc(collection(coll, email as string), data)
+    console.log("Successful addition!");
+  } catch (error) {
+    console.log(`I got an error! ${error}`);
+  }
 }
 
 /* Adds a new document */
 async function addFireUser(coll: DocumentReference, data: any) {
   try {
-  await addDoc(collection(coll, `${newUserUid}`), data)
-   console.log("Successful addition!");
+    await addDoc(collection(coll, `${newUserUid}`), data)
+    console.log("Successful addition!");
   } catch (error) {
     console.log(`I got an error! ${error}`);
   }
@@ -107,8 +108,8 @@ async function addFireUser(coll: DocumentReference, data: any) {
 /* Adds a new document */
 async function addFirePublic(coll: DocumentReference, data: any) {
   try {
-  await addDoc(collection(coll, "allPolls"), data)
-   console.log("Successful addition!");
+    await addDoc(collection(coll, "allPolls"), data)
+    console.log("Successful addition!");
   } catch (error) {
     console.log(`I got an error! ${error}`);
   }
@@ -116,6 +117,11 @@ async function addFirePublic(coll: DocumentReference, data: any) {
 
 
 const pollsPublicRef = collection(db, "polls/public/allPolls"); // Update collection reference to "allPolls"
+interface PollData {
+  id: string
+}[];
+
+let publicPollData = ref<PollData[]>([]);
 
 getDocs(pollsPublicRef).then((pollsSnapshot) => {
   const pollsPromises = pollsSnapshot.docs.map((pollsDoc) => {
@@ -124,18 +130,23 @@ getDocs(pollsPublicRef).then((pollsSnapshot) => {
   });
   return Promise.all(pollsPromises);
 })
-.then((pData) => {
-  console.log(pData);
-  // Access data from "allPolls" collection directly
-  if ('date' in pData[0]) {
-    console.log("PUBLIC DATE:", pData[0].date); // Code to get 'date' from document
-  }
-  numPolls = pData.length; // Code to get number of documents in "allPolls" subcollection
-  console.log(numPolls); // Code to get 'date' from document
-})
-.catch((err) => {
-  console.log(err.message);
-});
+  .then((pData) => {
+    console.log(pData);
+
+    for (let i = 0; i < pData.length; i++) {
+      publicPollData.value[i] = pData[i]
+    }
+
+    // Access data from "allPolls" collection directly
+    if ('date' in pData[0]) {
+      console.log("PUBLIC DATE:", pData[0].date); // Code to get 'date' from document
+    }
+    numPolls = pData.length; // Code to get number of documents in "allPolls" subcollection
+    console.log(numPolls); // Code to get 'date' from document
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 
 /* WORKING CODE FOR PULLING FROM POLLS/USER */
@@ -152,18 +163,18 @@ getDocs(pollsUserRef).then((pollsSnapshot) => {
   });
   return Promise.all(pollsPromises);
 })
-.then((pData) => {
-  const newUserIdArray = pData.map((data) => data.newUserId);
-  console.log(pData);
-  if ('date' in newUserIdArray[0][0]) {
-    console.log("USER DATE: ", newUserIdArray[0][0].date); //Code to get 'word' from document.
-  }
-//   console.log(newUserIdArray[0].length as number);
-  numPolls = newUserIdArray[0].length as number //Code to get number of documents in subcollection (USE FOR GAME NUMBER)
-})
-.catch((err) => {
-  console.log(err.message);
-});
+  .then((pData) => {
+    const newUserIdArray = pData.map((data) => data.newUserId);
+    console.log(pData);
+    if ('date' in newUserIdArray[0][0]) {
+      console.log("USER DATE: ", newUserIdArray[0][0].date); //Code to get 'word' from document.
+    }
+    //   console.log(newUserIdArray[0].length as number);
+    numPolls = newUserIdArray[0].length as number //Code to get number of documents in subcollection (USE FOR GAME NUMBER)
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 
 auth.onAuthStateChanged(setUserId);
@@ -171,117 +182,158 @@ logUserUid();
 console.log(`New value ${newUserUid}`)
 
 function plus() {
-    createPoll.value = 3 - createPoll.value;
-    pollData.pollQuestion = "";
-    pollData.pollChoices = ["", "", "", ""];
-    option3.value = 1;
-    option4.value = 1;
+  createPoll.value = 3 - createPoll.value;
+  pollData.pollQuestion = "";
+  pollData.pollChoices = ["", "", "", ""];
+  option3.value = 1;
+  option4.value = 1;
 }
 
 function op3() {
-    option3.value = 3 - option3.value;
-    if (option3.value == 1) {
-        pollData.pollChoices[2] = "";
-    }
-    if (option4.value == 2 &&  option3.value == 1) {
-        pollData.pollChoices[2] = pollData.pollChoices[3];
-        pollData.pollChoices[3] = "";
-        option3.value = 2;
-        option4.value = 1;
-    }
-    // if (option3.value == 2) {
-    //     pollData.pollChoices[2] = "";
+  option3.value = 3 - option3.value;
+  if (option3.value == 1) {
+    pollData.pollChoices[2] = "";
+  }
+  if (option4.value == 2 && option3.value == 1) {
+    pollData.pollChoices[2] = pollData.pollChoices[3];
+    pollData.pollChoices[3] = "";
+    option3.value = 2;
+    option4.value = 1;
+  }
+  // if (option3.value == 2) {
+  //     pollData.pollChoices[2] = "";
 
-    // }
-    // option4.value = 1;
+  // }
+  // option4.value = 1;
 }
 function op4() {
-    option4.value = 3 - option4.value;
-    if (option4.value == 1) {
-        pollData.pollChoices[3] = "";
-    }
-    // if (option4.value == 2 && option3.value == 1 && pollData.pollChoices[2] !== "") {
-    //     pollData.pollChoices[2] == pollData.pollChoices[3]
-    //     option3.value = 2;
-    //     option4.value = 1; 
-    // }
+  option4.value = 3 - option4.value;
+  if (option4.value == 1) {
+    pollData.pollChoices[3] = "";
+  }
+  // if (option4.value == 2 && option3.value == 1 && pollData.pollChoices[2] !== "") {
+  //     pollData.pollChoices[2] == pollData.pollChoices[3]
+  //     option3.value = 2;
+  //     option4.value = 1; 
+  // }
 }
 
-function post() {
-    createPoll.value = 3 - createPoll.value;
-    option3.value = 1;
-    option4.value = 1;
-    pollData.date = dt_string;
-    pollData.pollsCreated = numPolls + pollCount;
-    pollCount = pollCount + 1;
-    addFireUser(pollUser, pollData);
-    addFirePublic(pollPublic, pollData);
+// function post() {
+//   createPoll.value = 3 - createPoll.value;
+//   option3.value = 1;
+//   option4.value = 1;
+//   pollData.date = dt_string;
+//   pollData.pollsCreated = numPolls + pollCount;
+//   pollCount = pollCount + 1;
+//   addFireUser(pollUser, pollData);
+//   addFirePublic(pollPublic, pollData);
+//   setTimeout(() => {
+//     window.location.reload();
+//   }, 1000);
+
+// }
+
+async function post() {
+  createPoll.value = 3 - createPoll.value;
+  option3.value = 1;
+  option4.value = 1;
+  pollData.date = dt_string;
+  pollData.pollsCreated = numPolls + pollCount;
+  pollCount = pollCount + 1;
+
+  // Wait for both promises to resolve before reloading the page
+  await Promise.all([
+    addFireUser(pollUser, pollData),
+    addFirePublic(pollPublic, pollData),
+  ]);
+  window.location.reload();
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 }
 
 </script>
 
 <template>
-    <span v-if="isLoggedIn">
+  <span v-if="isLoggedIn">
+    <div class="header">
+      <span v-if="createPoll == 1">
+        <button @click="plus"> + </button>
+      </span>
+      <span v-if="createPoll == 2">
+        <button @click="plus"> Cancel </button>
+        <h1><input type="text" placeholder="Poll Question" v-model="pollData.pollQuestion" class="question" /></h1>
+        <h1><input type="text" placeholder="Poll Answer" v-model="pollData.pollChoices[0]" class="answer" /></h1>
+        <h1><input type="text" placeholder="Poll Answer" v-model="pollData.pollChoices[1]" class="answer" /></h1>
+        <span v-if="option3 == 1">
+          <button @click="op3"> + </button>
+        </span>
+        <span v-if="option3 == 2">
+          <div>
+            <input style="margin-right: 20px; margin-left: 66px;" type="text" placeholder="Poll Answer"
+              v-model="pollData.pollChoices[2]" class="answer" />
+            <button @click="op3"> - </button>
+          </div>
+        </span>
+        <br>
+        <span v-if="option4 == 1 && option3 == 2">
+          <button @click="op4"> + </button>
+        </span>
+        <span v-if="option4 == 2">
+          <div>
+            <input style="margin-right: 20px; margin-left: 66px;" type="text" placeholder="Poll Answer"
+              v-model="pollData.pollChoices[3]" class="answer" />
+            <button @click="op4"> - </button>
+          </div>
+        </span>
+        <br>
+        <br>
+        <br>
         <div class="header">
-            <span v-if="createPoll == 1">
-                <button @click="plus"> + </button>
-            </span>
-            <span v-if="createPoll == 2">
-                <button @click="plus"> Cancel </button>
-                <h1><input type="text" placeholder= "Poll Question" v-model="pollData.pollQuestion" class="question" /></h1>
-                <h1><input type="text" placeholder= "Poll Answer" v-model="pollData.pollChoices[0]" class="answer" /></h1>
-                <h1><input type="text" placeholder= "Poll Answer" v-model="pollData.pollChoices[1]" class="answer" /></h1>
-                <span v-if="option3 == 1">
-                    <button @click="op3"> + </button>
-                </span>
-                <span v-if="option3 == 2">
-                    <div>
-                        <input style="margin-right: 20px; margin-left: 66px;" type="text" placeholder= "Poll Answer" v-model="pollData.pollChoices[2]" class="answer" />
-                        <button @click="op3"> - </button>
-                    </div>
-                </span>
-                <br>
-                <span v-if="option4 == 1 && option3 == 2">
-                    <button @click="op4"> + </button>
-                </span>
-                <span v-if="option4 == 2">
-                    <div>
-                        <input style="margin-right: 20px; margin-left: 66px;" type="text" placeholder= "Poll Answer" v-model="pollData.pollChoices[3]" class="answer" />
-                        <button @click="op4"> - </button>
-                    </div>
-                </span>
-                <br>
-                <br>
-                <br>
-                <div class="header">
-                    <button @click="post"> Post </button>
-                </div>
-            </span>
+          <button @click="post"> Post </button>
         </div>
-        <div v-if="createPoll == 1">
-            <h1> Click the + button to create a poll! </h1>
-        </div>
-    </span>
+      </span>
+    </div>
+    <div v-if="createPoll == 1">
+      <h1 style="line-height: 100%;"> Click the + button to create a poll! </h1>
+      <div v-for="(poll, index) in publicPollData" :key="index">
+        <h2 class="pollQuestion">{{ poll.pollQuestion }}</h2>
+        <div v-for="(options) in poll.pollChoices">
+          <button class="pollButtons" v-if="options !== ''">{{ options }}</button>
+        </div><br>
+      </div>
+      <div>
+        <button @click="scrollToTop" class="back-to-top">Back to top</button>
+      </div>
+    </div>
+  </span>
 </template>
 
 
 <style scoped>
 /* @import url('../style.css'); */
 #grid {
-   display: inline-grid;
-   grid-template-columns: repeat(5, 1fr);
-   grid-template-rows: repeat(6, 1fr);
-   grid-gap: 8px;
- }
- .question {
-  border: 2px solid #3498db;
-  border-radius: 5px;
+  display: inline-grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  grid-gap: 8px;
+}
+
+.question {
+  border-radius: 8px;
+  border: 2px solid transparent;
+  border-color: #3498db;
   padding: 10px;
   font-size: 24px;
   color: #3498db;
   outline: none;
   transition: border-color 0.2s ease-in-out;
-  width: 100%; /* Set desired width here */
+  width: 100%;
+  /* Set desired width here */
 }
 
 .question:hover,
@@ -290,32 +342,24 @@ function post() {
 }
 
 .answer {
-  border: 2px solid #3498db;
-  border-radius: 5px;
+  border-radius: 8px;
+  border: 2px solid transparent;
+  border-color: #3498db;
   padding: 10px;
   font-size: 24px;
   color: #3498db;
   outline: none;
   transition: border-color 0.2s ease-in-out;
-  width: 50%; /* Set desired width here */
+  width: 50%;
+  /* Set desired width here */
 }
 
 .answer:hover,
 .answer:focus {
   border-color: #1abc9c;
 }
- .cell {
-   text-transform: uppercase;
-   text-align: center;
-   width: 100px;
-   height: 100px;
-   align-self: center;
-   font-size: 100px;
-   border: 5px solid black;
-   color: black;
-   background-color: white;
- }
- .header button {
+
+.header button {
   border-radius: 8px;
   border: 2px solid transparent;
   padding: 0.15em .5em;
@@ -327,44 +371,55 @@ function post() {
   cursor: pointer;
   transition: border-color 0.25s;
 }
- .report {
-   background-color: darkseagreen;
-   padding: 7px;
-   font-size: 20px;
-   border: 5px dashed black;
-   color: black;
- }
 
- #wrong {
-   background-color: grey;
- }
+.report {
+  background-color: darkseagreen;
+  padding: 7px;
+  font-size: 20px;
+  border: 5px dashed black;
+  color: black;
+}
 
- #right {
-   background-color: green;
- }
+#wrong {
+  background-color: grey;
+}
 
- #misplaced {
-   background-color: rgb(186, 186, 3);
- }
+#right {
+  background-color: green;
+}
 
- .buttons {
-   display: flex;
-   align-items: center;
-   justify-content: center;
-   grid-gap: 8px;
- }
+#misplaced {
+  background-color: rgb(186, 186, 3);
+}
 
- #timer {
-   display: inline-block;
-   border: 5px solid brown;
-   border-radius: 8px;
-   width: 120px;
-   text-align: center;
-   padding-bottom: 3px;
-   color: black;
- }
+.buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  grid-gap: 8px;
+}
 
- #timedisplay {
-   font-size: 280%;
- }
+.pollButtons {
+  border-radius: 8px;
+  border: 2px solid transparent;
+  border-color: #3498db;
+  display: inline-block;
+  width: 30%;
+  justify-content: center;
+  margin: 2px;
+  color: black;
+}
+
+.pollQuestion {
+  border-radius: 8px;
+  border: 2px solid transparent;
+  border-color: #3498db;
+  padding: 1.5%;
+  display: inline-block;
+  width: fit-content;
+  justify-content: center;
+  margin-top: 5%;
+  color: black;
+}
+
 </style>
